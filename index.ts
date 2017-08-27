cb7375d652abec1b4a40c6197d103e3ff6b0e046
Gilad Kutiel <gilad.kutiel@gmail.com>
2017-08-27 15:59:12 +0300

    fix

--- index.ts
+++ index.ts
@@ -4,11 +4,17 @@ export const load = (jumpFm: JumpFm) => {
     const html = document.querySelector('html')
     let fontSize = 14
 
+    html.style.fontSize = `${fontSize}px`
+
+    jumpFm.bind('fontSizeInc', ['ctrl+0'], () => {
+        html.style.fontSize = `${fontSize = 14}px`
+    })
+
     jumpFm.bind('fontSizeInc', ['ctrl+='], () => {
-        html.style.fontSize = `${fontSize++}px`
+        html.style.fontSize = `${++fontSize}px`
     })
 
     jumpFm.bind('fontSizeDec', ['ctrl+-'], () => {
-        html.style.fontSize = `${fontSize--}px`
+        html.style.fontSize = `${--fontSize}px`
     })
 }
